4a4907842ee7feb426666a66b758092d163a53c9
Joel Lozano <joel.lozano@rappi.com>
2023-07-27 20:37:23 -0600

    cors

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 //import  express  from "express";
 const express = require('express')
 const routes = require('./Route/routes')
+var cors = require('cors')
 require('dotenv').config()
 
 const app = express();
-
+app.use(cors())
 app.use(routes);
 app.use(express.json());
 
